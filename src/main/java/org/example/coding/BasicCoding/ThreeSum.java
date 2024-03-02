9dda8226b95238f661dc91e66b6fb6b572bf1535
ayush <ayushsnghcool@gmail.com>
2024-03-02 18:52:50 +0530

    added optimal solution for three sum

--- src/main/java/org/example/coding/BasicCoding/ThreeSum.java
+++ src/main/java/org/example/coding/BasicCoding/ThreeSum.java
@@ -3,6 +3,8 @@ package org.example.coding.BasicCoding;
 import java.util.*;
 
 public class ThreeSum {
+
+    //Brute Force
     public List<List<Integer>> threeSum(int[] nums) {
         Set<Integer> set = new HashSet<>();
         List<List<Integer>> result = new ArrayList<>();
@@ -20,9 +22,34 @@ public class ThreeSum {
         return result;
     }
 
+    //Optimal Solution
+
+    public List<List<Integer>> optimalThreeSum(int[] nums){
+
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for(int i = 0 ; i< nums.length-2; i++){
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum==0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                }
+                else if(sum < 0)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
     public static void main(String[] args) {
         int[] arr = {-1, 0, 1, 2, -1, -4};
         ThreeSum threeSum = new ThreeSum();
-        System.out.println(threeSum.threeSum(arr));
+        System.out.println(threeSum.optimalThreeSum(arr));
     }
 }
